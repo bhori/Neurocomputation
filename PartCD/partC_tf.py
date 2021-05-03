3ce4097f0e323907078b37758064f53fb3086545
bhori <bhorib@gmail.com>
2021-05-03 23:28:41 +0300

    First version of the model

--- PartCD/partC_tf.py
+++ PartCD/partC_tf.py
@@ -1,20 +1,43 @@
 import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras import datasets, layers, models
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 import glob
 
+
+def print_data(data, labels):
+    for i in range(1000):
+        if labels[i][0]==1:
+            print(data[i][0], ",", data[i][1],  "==>", labels[i][0])
+
 data_x = np.load('../train.npy')
-data_y = np.load('../lables.npy')
-data_y = data_y.flatten()
+labels = np.load('../lables.npy')
+# print_data(data_x, labels)
+# labels = labels.flatten()
+
 test_data = np.load('../test_data.npy')
 test_labels = np.load('../test_labels.npy')
-test_labels = test_labels.flatten()
-model = models.Sequential()
-model.add(layers.Dense(2, activation='relu', input_shape=(2, 2)))
-# model.add(layers.Dropout(0.5))
-model.add(layers.Dense(1))
+# print_data(test_data, test_labels)
+# test_labels = test_labels.flatten()
+
+# model = keras.models.Sequential()
+#
+# original_inputs = tf.keras.Input(shape=(2,), name="encoder_input")
+# x = layers.Dense(2, activation="relu")(original_inputs)
+# model = keras.Model()
+
+# model = keras.models.Sequential()
+# model.add(keras.Input(shape=(2,)))
+# model.add(layers.Dense(100, activation='relu')) # , input_shape=(2, )
+# model.add(layers.Dense(1))
+
+inputs = tf.keras.Input(shape=(2,))
+x = tf.keras.layers.Dense(100, activation='relu')(inputs)
+outputs = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+model = tf.keras.Model(inputs=inputs, outputs=outputs)
+
 
 model.summary()
 
@@ -22,7 +45,7 @@ model.compile(optimizer='adam',
                   loss=tf.keras.losses.MeanSquaredError(),
                   metrics=['accuracy'])
 
-history = model.fit(x=data_x, y=data_y, batch_size=None, epochs=400)
+history = model.fit(x=data_x, y=labels, epochs=400)
 
 # Show results in graph view
 plt.plot(history.history['accuracy'], label='accuracy')
